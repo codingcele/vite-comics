94df55f640f824f1fc3f0946cfdfd823080ca627
codingcele <marcocelegato9@gmail.com>
2022-12-13 16:04:40 +0100

    primo commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <header>
+        <a href="/" class="brand">
+            <img src="/dc-logo.png" alt="DC">
+        </a>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+/* scoped serve a dire che il css si riferirà solo all'header */
+header {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
